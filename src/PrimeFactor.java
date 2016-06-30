d238311b72be232c5676342f22e5f92c8442d8a6
Alex Jensen <alex@Johns-MacBook-Pro.local>
2016-06-30 14:41:35 -0500

    .java ready to compile

--- src/PrimeFactor.java
+++ src/PrimeFactor.java
@@ -1,5 +1,22 @@
+import java.util.*;
+
 public class PrimeFactor {
-    public int getPrimes(int num){
-        return num;
+
+    public ArrayList getPrimes(int num){
+        ArrayList factors = new ArrayList();
+
+        for(int i = 2;i <= num;i++){
+            while(num % i == 0){
+                factors.add(i);
+                num /= i;
+            }
+        }
+        return factors;
+    }
+
+    public static void main(String args[]){
+        PrimeFactor factorGen = new PrimeFactor();
+        System.out.println(args[0]);
+        System.out.println(factorGen.getPrimes(Integer.parseInt(args[0])));
     }
 }
--- src/PrimeFactorTest.java
+++ src/PrimeFactorTest.java
@@ -1,10 +1,44 @@
 import static org.junit.Assert.*;
+import java.util.*;
 
 public class PrimeFactorTest {
     PrimeFactor primeGenerator = new PrimeFactor();
 
     @org.junit.Test
-    public void oneReturnsOne(){
-        assertEquals(primeGenerator.getPrimes(1),1);
+    public void oneReturnsNil(){
+        ArrayList expected = new ArrayList();
+        assertEquals(expected,primeGenerator.getPrimes(1));
+    }
+
+    @org.junit.Test
+    public void twoReturnsFactorsOfTwo(){
+        ArrayList expected = new ArrayList();
+        expected.add(2);
+        assertEquals(expected,primeGenerator.getPrimes(2));
+    }
+
+    @org.junit.Test
+    public void nineReturnsFactorsOfNine(){
+        ArrayList expected = new ArrayList();
+        expected.add(3);
+        expected.add(3);
+        assertEquals(expected,primeGenerator.getPrimes(9));
+    }
+
+    @org.junit.Test
+    public void twelveReturnsFactorsOfTwelve(){
+        ArrayList expected = new ArrayList();
+        expected.add(2);
+        expected.add(2);
+        expected.add(3);
+        assertEquals(expected,primeGenerator.getPrimes(12));
+    }
+
+    @org.junit.Test
+    public void ThirteenSquared(){
+        ArrayList expected = new ArrayList();
+        expected.add(13);
+        expected.add(13);
+        assertEquals(expected,primeGenerator.getPrimes(169));
     }
 }
